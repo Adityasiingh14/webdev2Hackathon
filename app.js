a0089c702a30d4e3f771ec0f8430fdf04c8d6ad4
Adityasiingh14 <140969491+Adityasiingh14@users.noreply.github.com>
2024-02-10 18:13:06 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-let submitbtn = document.querySelector(".submitbtn");
-submitbtn.addEventListener("click",function(){
-    var commentboxval =  document.getElementsByClassName("commentbox-text").value;
-    if(commentboxval == ""){
-        alert("Please enter a valid comment!");
-    }
-
-})
